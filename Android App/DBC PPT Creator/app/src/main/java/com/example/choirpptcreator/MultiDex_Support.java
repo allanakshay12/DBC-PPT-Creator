7a78903591efd2c5b43325e66bfe3ef678d1ed23
Jerry Allan Akshay <allanakshay@outlook.com>
2020-04-04 23:57:43 +0530

    Final Commit

---
+++ Android App/DBC PPT Creator/app/src/main/java/com/example/choirpptcreator/MultiDex_Support.java
@@ -0,0 +1,27 @@
+package com.example.choirpptcreator;
+
+import android.app.Application;
+import android.content.Context;
+
+import androidx.multidex.MultiDex;
+
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MultiDex_Support extends Application {
+    public MultiDex_Support() {
+
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        FirebaseDatabase.getInstance().setPersistenceEnabled(false);
+    }
+
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
+
+
+    }
+}
